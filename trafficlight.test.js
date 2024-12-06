53b40af028b41a3ae131f5fa1ac7a3ce11571067
amelta <96289091+amelta@users.noreply.github.com>
2024-12-06 11:10:59 +0100

    Update trafficlight.test.js

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -4,4 +4,13 @@ describe('test suite traffic light', () => {
     it(`case red light`, () => {
         expect(app.trafficLight("red")).toBe("DON'T WALK!");
     });
+    it(`case orange light`, () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+    it(`case green light`, () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+    it(`case green light`, () => {
+        expect(app.trafficLight(undefined)).toBe("OUT OF ORDER");
+    });
 });
